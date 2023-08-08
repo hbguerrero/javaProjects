3d8e8855f4d1b8f03ef9dec982637f79c98ea135
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-08 13:36:07 -0500

    Add files via upload

---
+++ project2/Add.java
@@ -0,0 +1,15 @@
+package add;
+
+public class Add {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10;    
+        int b=10;    
+        int c=a+b;    
+        System.out.println(c);    
+    }
+    
+}
