048e212c7e4f65c7675051e9926a684c9f6699ed
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-13 11:59:23 -0500

    Add files via upload

---
+++ ConditionalExamples.java
@@ -0,0 +1,13 @@
+package conditionalexamples;
+
+public class ConditionalExamples {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;    
+        int y = 12;    
+        if(x+y > 20) {    
+            System.out.println("x + y is greater than 20");    
+        }    
+    }      
+}
