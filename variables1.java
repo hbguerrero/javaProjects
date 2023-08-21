10d0d798c42de707e53a6df41b3a8e1cef77f8e7
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 16:27:34 -0500

    Create variables1.java

---
+++ variables1.java
@@ -0,0 +1,8 @@
+public class Creator {
+	public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
