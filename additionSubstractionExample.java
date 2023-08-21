da8d6dae7e14ebeedbe7786661f36a0aaaef81b7
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 17:50:02 -0500

    Create additionSubstractionExample.java

---
+++ additionSubstractionExample.java
@@ -0,0 +1,15 @@
+public class PlusAndMinus {
+	public static void main(String[] args) {   
+		int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    
+    System.out.println(animalsInZoo); 
+
+    int numZebrasAfterTrade = zebrasInZoo - 2;
+
+    System.out.println(numZebrasAfterTrade);
+
+
+	}
+}
