68c01983830d4f6e4bf38045d4fffe7cfc6c7d62
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 16:07:12 -0500

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,16 @@
+class Tree  
+{    
+    public static void main(String args[]) {  
+       System.out.println("Hey there, I’m Henry and I’m learning to code in Java!");
+       System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println("*******");
+       System.out.println(" *****  ");
+       System.out.println("  ***  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+   }  
+}  
