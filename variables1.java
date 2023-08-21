8103715e07e872ef5c0bec5547276f2ca220e958
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 16:31:39 -0500

    Update variables1.java

--- variables1.java
+++ variables1.java
@@ -1,7 +1,7 @@
 public class Creator {
 	public static void main(String[] args) {
     String name = "James Gosling";
-    int yearCreated = 1995;
+    int yearCreated = 1995;      //The int data type allows values between -2,147,483,648 and 2,147,483,647, inclusive.
     System.out.println(name);
     System.out.println(yearCreated);
 	}
