77014364bd7614d51ea89ad35a0ef42658c49e67
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 17:12:09 -0500

    Create stringExample.java

---
+++ stringExample.java
@@ -0,0 +1,6 @@
+public class Song {
+	public static void main(String[] args) {   
+    String openingLyrics = "Yesterday, all my troubles seemed so far away";
+    System.out.println(openingLyrics);
+	}
+}
