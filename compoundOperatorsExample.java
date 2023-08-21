957f0b06dfb0d90013a6c05f32e12f333fe790af
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 18:03:19 -0500

    Create compoundOperatorsExample.java

---
+++ compoundOperatorsExample.java
@@ -0,0 +1,19 @@
+//Compound operators:
+/*
+Addition (+=)
+Subtraction (-=)
+Multiplication (*=)
+Division (/=)
+Modulo (%=)
+*/ 
+
+public class BakeSale {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+    numCookies -= 3;
+    System.out.println(numCookies);
+    numCookies /= 2;
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
