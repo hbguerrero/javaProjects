d2407885b53314f38611dbd832c71720944559e4
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 16:37:27 -0500

    Create doubleExample.java

---
+++ doubleExample.java
@@ -0,0 +1,10 @@
+public class MarketShare {
+	public static void main(String[] args) {
+    double androidShare = -81.7;
+    /*
+    double can hold decimals as well as very large and very small numbers. The maximum value is 1.797,693,134,862,315,7 E+308, which is approximately 17 followed by 307 zeros. The minimum value is 4.9 E-324, which is 324 decimal places!
+    */   
+   
+    System.out.println(androidShare);
+	}
+}
