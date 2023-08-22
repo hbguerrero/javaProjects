03b2f60b7f10bb59cb5553a2d72307ac972b67a1
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 20:47:18 -0500

    Create finalKeywordExample1.java

---
+++ finalKeywordExample1.java
@@ -0,0 +1,8 @@
+public class Final {
+	public static void main(String[] args) { 
+    final double pi = 3.14;
+    System.out.println(pi);  
+    pi = 2*pi;  //Note that pi is unchangable
+		
+	}
+}
