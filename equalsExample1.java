64b8bac85038e27cf44653f7863e1ae10b55f0c8
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 19:41:49 -0500

    Create equalsExample1.java

---
+++ equalsExample1.java
@@ -0,0 +1,10 @@
+public class Song {
+  	public static void main(String[] args){
+      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+
+      System.out.println(line1.equals(line2));
+      System.out.println(line2.equals(line3));
+    }       
+}
