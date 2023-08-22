6e2beed39397d7985709c38f5211aa5401297b37
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 19:25:11 -0500

    Create relationalOperatorsExample2.java

---
+++ relationalOperatorsExample2.java
@@ -0,0 +1,10 @@
+public class EqualNotEqual {
+	public static void main(String[] args) {   
+		int songsA = 9;
+    int songsB = 9;
+    int albumLengthA = 41;
+    int albumLengthB = 53;
+    boolean sameNumberOfSongs = songsA == songsB;
+    boolean differentLength = albumLengthA != albumLengthB;
+	}
+}
