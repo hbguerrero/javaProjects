892f675ac5a445519613fd883a6f7604335ca42b
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 19:08:31 -0500

    Create relationalOperatorsExample1.java

---
+++ relationalOperatorsExample1.java
@@ -0,0 +1,10 @@
+public class GreaterLessThan {
+	public static void main(String[] args) {   
+		double creditsEarned = 176.5;
+    double creditsOfSeminar = 8;
+    double creditsToGraduate = 180;
+    double creditsAfterSeminar = creditsEarned + creditsOfSeminar; 
+    System.out.println(creditsEarned > creditsToGraduate);
+    System.out.println(creditsToGraduate < creditsAfterSeminar);
+	}
+}
