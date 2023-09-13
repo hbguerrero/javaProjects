05b1efc9e25d9a14ef51f11c2138518e89a3de74
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-09-13 09:12:32 -0500

    Add files via upload

---
+++ AccessEncapsulationScope/Bank/CheckingAccount.java
@@ -0,0 +1,28 @@
+public class CheckingAccount{
+  private String name;
+  private int balance;
+  private String id;
+  private double interestRate;
+
+  public CheckingAccount(String inputName, int inputBalance, String inputId){
+    this.name = inputName;
+    this.balance = inputBalance;
+    this.id = inputId;
+    this.interestRate = 0.02;
+  }
+
+  public int getBalance(){
+    return this.balance;
+  }
+  
+  public void setBalance(int newBalance){
+    this.balance = newBalance;
+  }
+
+  public double getMonthlyInterest(){
+    return this.interestRate * this.balance;
+  }
+
+public static void main(String[] args){   
+  }
+}
